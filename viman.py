61b16f09dde232a4decdb07df6bd4197fb0947e7
Shylock Hg <tcath2s@gmail.com>
2018-07-31 16:55:15 +0800

    implement low git operation

--- viman.py
+++ viman.py
@@ -2,24 +2,98 @@
 
 import argparse
 import sys
+import subprocess
+import os
+
+errno = ['OK',                          #0
+        '~/.vim/bundle don\'t exists!'] #1
 
 class vimanArgParser(argparse.ArgumentParser):
     def __init__(self,*args,**kwargs):
         super().__init__(...)
         # base option
+        # Synchronize and option
         self.add_argument('-S','--synchronize',help='Synchronize plugin!')
+        self.add_argument('-u','--sysupgrade',action='store_true',help='Upgrade all plugin!')
+
+        #Remove and option
         self.add_argument('-R','--remove',help='Remove plugin!')
+
+        #Query and option
         self.add_argument('-Q','--query',help='Query local plugin!')
+
+        #Upgrade and option
         self.add_argument('-U','--upgrade',help='Upgrade local plugin!')
+
+        #Version
         self.add_argument('-V','--version',action='version',version='%(prog)s 0.0.1')
 
+class vimanGitWrapper():
+    '''
+    @brief low operation wrapper of git , install , upgrade , remove , query 
+    @note all other operation base on this
+    '''
+
+    DIR_PLUGIN = '~/.vim/bundle'
+
+    @staticmethod
+    def install(url):
+        '''
+        @brief install a vim plugin to .vim/bundle by git from url
+        @param url url of git repository
+        '''
+        if not os.path.isdir(DIR_PLUGIN):
+            sys.exit()
+        sys.chdir(DIR_PLUGIN)
+        sys.exit(subprocess.run(['git','clone',url]))
+
+    @staticmethod
+    def upgrade(url):
+        '''
+        @brief upgrade a vim plugin by git from url
+        @param url url of git repository
+        '''
+        upgradeByName(_getPlugin4Url(url)
+        
+    @staticmethod
+    def upgradeByName(name):
+        path = sys.path.join(DIR_PLUGIN,name)
+        if not sys.path.isdir(path):
+            sys.exit()
+        sys.chdir(path)
+        sys.exit(subprocess.run(['git','pull']))
+
+    @staticmethod
+    def remove(url):
+        '''
+        @brief remove a vim plugin from url
+        @param url url of git repository
+        '''
+        removeByName(_getPlugin4Url(url))
+
+    @staticmethod
+    def removeByName(name):
+        path = sys.path.join(DIR_PLUGIN,name)
+        if not sys.path.isdir(path):
+            sys.exit()
+        sys.exit(subprocess.run(['rm','-rf',path]))
+
+    @staticmethod
+    def _getPlugin4Url(url):
+        token_url = url.split('/')  # https://github.com/user/repository.git
+        token_name = token_url[-1].split('.') # repository.git
+        return token_name[0]
 
 def main():
+    #parse argumenits
     argparser = vimanArgParser(prog='viman')
     args = argparser.parse_args()
 
+    if args.synchronize :
+
+    elif args.remove :
 
 
-if '__main__' == __name__:
-    main()
+    if '__main__' == __name__:
+        main()
 

f9a18466d47cb052cfa6a45d3898902c470b5c76
shylock <tcath2s@gmail.com>
2018-08-26 18:46:48 +0800

    Find version and name by one function

--- setup.py
+++ setup.py
@@ -8,21 +8,21 @@ def readme():
 
 LONG_DESCRIPTION = open('README.rst').read()
 
-def find_version(file_path):
+def find_attr(attr, file_path):
     with open(file_path) as f:
-        version_file = f.read()
-    version_match = re.search(
-        r"^__version__\s*=\s*['\"]([^'\"]*)['\"]",
-        version_file,
-        re.M,
-    )
-    if version_match:
-        return version_match.group(1)
+        attr_file = f.read()
+    attr_match = re.search(
+            r"^{}\s*=\s*['\"]([^'\"]*)['\"]".format(attr),
+            attr_file,
+            re.M)
+
+    if attr_match:
+        return attr_match.group(1)
 
     raise RuntimeError("Unable to find version string.")
 
-setup(name='viman',
-        version=find_version('viman/__init__.py'),
+setup(name=find_attr('__program__', 'viman/__init__.py'),
+        version=find_attr('__version__', 'viman/__init__.py'),
         description='The vim plugin manager based on git and pathogen!',
         long_description = LONG_DESCRIPTION,
         url='https://github.com/Shylock-Hg/viman.git',

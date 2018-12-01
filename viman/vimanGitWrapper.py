158874fb7d39b1107e16dcbca3b9f094c3c315c8
Shylock-Hg <tcath2s@gmail.com>
2018-12-01 04:32:04 +0000

    Optimize get $HOME by cross-platfrom API instead of getenv.

--- viman/vimanGitWrapper.py
+++ viman/vimanGitWrapper.py
@@ -21,7 +21,7 @@ class vimanGitWrapper():
     @note all other operation base on this
     '''
 
-    DIR_PLUGIN = os.path.join(os.getenv('HOME'), '.vim/bundle')
+    DIR_PLUGIN = os.path.join(os.path.expanduser('~'), '.vim/bundle')
 
     @staticmethod
     @vimanYamlWrapper.vimanYamlWrapper.installWrapper

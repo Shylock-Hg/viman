9741dea2c49d73b72c9ba185dfa74059566bc3db
Shylock-Hg <tcath2s@gmail.com>
2018-12-04 03:41:45 +0000

    Optimize testing by unittest:   1.  Testing cli usage in unittesting.   2.  Using unittest script instead dry command in travis-ci.

---
+++ testing.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python3
+
+'''
+@brief
+@author Shylock Hg
+@date 2018-12-04
+@email tcath2s@gmail.com
+'''
+
+import unittest
+
+from viman import vimanApp
+
+class cli_tests(unittest.TestCase):
+    argv_set = [
+        ['viman', '-h'],
+        ['viman', '-V'],
+        ['viman', '-Q'],
+        ['viman', '-Sf', 'test.yml'],
+        ['viman', '-S', 'https://github.com/vimscript/vim-snippets.git'],
+        ['viman', '-Ur'],
+        ['viman', '-R', 'https://github.com/vimscript/vim-snippets.git'],
+        ['viman', '-Rn', 'a.vim'],
+        ['viman', '-Rf', '~/.viman.yml'],
+    ]
+
+    def test_cli(self):
+        for argv in cli_tests.argv_set:
+            self.assertEqual(vimanApp.main(argv), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
